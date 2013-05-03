15869c5a7b28f52707b2c4ddc1764fe00815ad0f
corey spangler <corey@therushstudio.com>
2013-05-03 15:50:41 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fund::Application.config.secret_token = 'a8ae6129aa3582b3ec06d1557745ea791315ed02f3f4c17bd8a03bfc9ff7f3171c6a8ca94e12a86e78fced1620b07ab79bc92d3babd6224309a84b0d5d467b62'
